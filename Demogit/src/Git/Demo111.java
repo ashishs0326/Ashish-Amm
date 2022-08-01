1fd5a40ec66d2ac737d12df06341238985323678
Sony <Sony@DESKTOP-OJ3AVQH>
2022-08-01 18:25:28 +0530

    cjc

---
+++ Demogit/src/Git/Demo111.java
@@ -0,0 +1,12 @@
+package Git;
+
+public class Demo111 {public static void main(String[] args) {
+	
+	System.out.println("This is Git Bhai");
+	
+	System.out.println("Maari Bhaai");
+	System.out.println("Kaat Daalungaaaaa");
+}
+
+
+}
